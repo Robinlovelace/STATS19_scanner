822bcedd3be676829fb5c1aee1e295b99b41241e
Henry Partridge <henrypartridge@Henrys-MacBook-Air.local>
2015-07-03 11:17:03 +0100

    Changed to ggvis outputs

--- app.R
+++ app.R
@@ -1,5 +1,5 @@
 library(shiny) ; library(shinydashboard) ; library(dplyr) ; library(rgdal) ; library(leaflet) ; 
-library(RColorBrewer) ; library(ggplot2) ; library(scales) ; library(DT)
+library(RColorBrewer) ; library(ggvis) ; library(scales) ; library(DT)
 
 # load the casualty data
 data <- readRDS(file="casualties_2005-14.Rda")
@@ -16,12 +16,14 @@ ui <- shinyUI(dashboardPage(skin = "blue",
                             dashboardHeader(title = "STATS19 scanner"),
                             dashboardSidebar(
                               sidebarMenu(
-                                selectInput("year", 
-                                            label = "Select year:",
-                                            choices = c(levels(data$year)),
-                                            selected = 2014),
+                                sliderInput("year",
+                                            label = "Select a year:",
+                                            min = 2005,
+                                            max = 2014,
+                                            value = 2014,
+                                            sep = "", ticks = FALSE),
                                 radioButtons(inputId = "mode",
-                                             label = "Select mode of travel:",
+                                             label = "Select a mode of travel:",
                                              choices = c(levels(data$mode)),
                                              selected = "Pedal Cycle"),
                                 selectInput(inputId = "severity",
@@ -30,6 +32,7 @@ ui <- shinyUI(dashboardPage(skin = "blue",
                                             selected = c(levels(data$severity)), multiple=T),
                                 hr(),
                                 menuItem("Casualty map", tabName = "map", icon = icon("fa fa-map-marker")),
+                                menuItem("Borough statistics", tabName = "boroughs", icon = icon("fa fa-bar-chart")),
                                 menuItem("Temporal profile", tabName = "temporal", icon = icon("fa fa-clock-o")),
                                 menuItem("Demographics", tabName = "demographics", icon = icon("fa fa-users")),
                                 menuItem("Raw data", tabName = "data", icon = icon("th")),
@@ -40,26 +43,29 @@ ui <- shinyUI(dashboardPage(skin = "blue",
                                 tabItem(tabName = "map",
                                         fluidRow(
                                           title = "Map", width = 12,
-                                          leafletOutput("map", height = "700"))
+                                          leafletOutput("map", height = "1000"))
                                 ),
                                 tabItem(tabName = "boroughs",
                                         fluidRow(
-                                          box(title = "Casualties by borough and severity", width = 12, solidHeader=TRUE, collapsible=TRUE,
-                                              plotOutput("borough")))
+                                          box(title = "Top 10 boroughs", height = "auto", width = 2, solidHeader=TRUE, collapsible=TRUE,
+                                              tableOutput("borough_stats")),
+                                          box(title = "Casualties by borough", width = 10, solidHeader=TRUE, collapsible=TRUE,
+                                              ggvisOutput("borough_count")))
+                                        
                                 ),
                                 tabItem(tabName = "temporal",
                                         fluidRow(
                                           box(title = "Casualties by month and severity", width = 5, solidHeader=TRUE, collapsible=TRUE,
-                                              plotOutput("timeband_month")),
+                                              ggvisOutput("timeband_month")),
                                           box(title = "Casualties by hour and light conditions", width = 7, solidHeader=TRUE, collapsible=TRUE,
-                                              plotOutput("timeband_hour")))
+                                              ggvisOutput("timeband_hour")))
                                 ),
                                 tabItem(tabName = "demographics",
                                         fluidRow(
                                           box(title = "Casualties by ageband and gender", width = 6, solidHeader=TRUE, collapsible=TRUE,
-                                              plotOutput("ageband_gender")),
+                                              ggvisOutput("ageband_gender")),
                                           box(title = "Casualties by ageband and severity", width = 6, solidHeader=TRUE, collapsible=TRUE,
-                                              plotOutput("ageband_severity")))
+                                              ggvisOutput("ageband_severity")))
                                 ),
                                 tabItem(tabName = "data",
                                         fluidRow(box(width = 12, DT::dataTableOutput("table"))))
@@ -90,86 +96,112 @@ server <- function(input, output, session) {
       addCircles(~long, ~lat, radius = 6, color = ~pal(severity), fillOpacity = 0.3, opacity = 0.5, popup = ~text)
   })
   
+  
+  ## BOROUGH STATISTICS ## 
+  
+  # Casualties by borough
+  borough_count <- reactive({ 
+    casualties() %>%
+      group_by(borough) %>%
+      summarise(count = n()) %>% 
+      ggvis(~borough, ~count, fill := "steelblue") %>%
+      mutate(borough = reorder(borough, -count)) %>%
+      layer_bars(stroke := "white") %>%
+      add_axis("x", title = "", properties = axis_props(labels=list(angle=270, align="right"))) %>%
+      add_axis("y", title = "", format='d') %>%
+      hide_legend("fill") %>% 
+      set_options(width="auto") %>% 
+      add_tooltip(function(casualties) (casualties$stack_upr_ - casualties$stack_lwr_))
+  })
+  borough_count %>% bind_shiny("borough_count")  
+  
+  # Borough statistics
+  output$borough_stats <- renderTable({
+    dataset <- casualties()
+    dataset %>% 
+      group_by(borough) %>%
+      summarise(count = n()) %>%
+      ungroup() %>%
+      arrange(desc(count)) %>%
+      mutate(percent = paste0(round(100 * count/sum(count), 1), "%")) %>%
+      select(Borough = borough, Count = count, Percent = percent) %>% 
+      head(10)
+  }, include.rownames=FALSE)
+  
+  
   ## TEMPORAL PROFILE ##
   
   # Casualties by month and severity
-  output$timeband_month <- renderPlot({
-    timeband_month <- casualties() %>%
+  timeband_month <- reactive({ 
+    casualties() %>%
       group_by(severity, month) %>%
-      summarise(count = n())
-    ggplot(timeband_month, aes(x=month, y=count, fill=severity)) +
-      geom_bar(stat="identity") +
-      scale_y_continuous(breaks= pretty_breaks()) +
-      scale_fill_manual(values=c("black", "red", "orange"), name="") +
-      ylab("") + xlab("") +
-      theme_bw() +
-      theme(legend.position = "bottom",
-            axis.line = element_line(colour = "black"),
-            panel.border = element_rect(colour = NA),
-            axis.text.x = element_text(angle = 90, hjust = 1),
-            panel.grid.major.x = element_line(colour=NA))
+      summarise(count = n()) %>% 
+      ggvis(~month, ~count, fill = ~severity) %>%
+      layer_bars(stroke := "white") %>%
+      scale_nominal("fill", range = c("black", "red", "orange")) %>%
+      add_axis("x", title = "", properties = axis_props(labels=list(angle=270, align="right"))) %>%
+      add_axis("y", title = "", format='d') %>%
+      add_legend("fill", title = "Severity") %>%  
+      set_options(width="auto") %>% 
+      add_tooltip(function(casualties) (casualties$stack_upr_ - casualties$stack_lwr_))
   })
+  timeband_month %>% bind_shiny("timeband_month")
   
   # Casualties by hour and light conditions
-  output$timeband_hour <- renderPlot({
-    timeband_hour <- casualties() %>%
+  timeband_hour <- reactive({ 
+    casualties() %>%
       group_by(light, hour) %>%
-      summarise(count = n())
-    ggplot(timeband_hour, aes(x=hour, y=count, fill=light)) +
-      geom_bar(stat="identity") +
-      scale_y_continuous(breaks= pretty_breaks()) +
-      scale_fill_manual(values=c("midnightblue", "yellow"), name="") +
-      ylab("") + xlab("") +
-      theme_bw() +
-      theme(legend.position = "bottom",
-            axis.line = element_line(colour = "black"),
-            panel.border = element_rect(colour = NA),
-            axis.text.x = element_text(angle = 90, hjust = 1),
-            panel.grid.major.x = element_line(colour=NA))
-  })  
+      summarise(count = n()) %>% 
+      ggvis(~hour, ~count, fill = ~light) %>%
+      layer_bars(stroke := "white") %>%
+      scale_nominal("fill", range = c("midnightblue", "yellow")) %>%
+      add_axis("x", title = "", properties = axis_props(labels=list(angle=270, align="right"))) %>%
+      add_axis("y", title = "", format='d') %>%
+      add_legend("fill", title = "Light conditions") %>% 
+      set_options(width="auto") %>% 
+      add_tooltip(function(casualties) (casualties$stack_upr_ - casualties$stack_lwr_))
+  })
+  timeband_hour %>% bind_shiny("timeband_hour")
   
   ## DEMOGRAPHICS ##  
   
   # Casualties by ageband and gender
-  output$ageband_gender <- renderPlot({
-    ageband_gender <- casualties() %>%
+  ageband_gender <- reactive({ 
+    casualties() %>%
       group_by(sex, ageband) %>%
-      summarise(count = n())
-    ggplot(ageband_gender, aes(x=ageband, y=count, fill=sex)) +
-      geom_bar(position="dodge", stat="identity") +
-      scale_y_continuous(breaks= pretty_breaks()) +
-      scale_fill_manual(values=c("steelblue", "mediumpurple1"), name="", labels=c("Male", "Female")) +
-      ylab("") + xlab("") +
-      theme_bw() +
-      theme(legend.position = "bottom",
-            axis.line = element_line(colour = "black"),
-            panel.border = element_rect(colour = NA),
-            axis.text.x = element_text(angle = 90, hjust = 1),
-            panel.grid.major.x = element_line(colour=NA))
+      summarise(count = n()) %>% 
+      ggvis(~ageband, ~count, fill = ~sex) %>%
+      layer_bars(stroke := "white") %>%
+      scale_nominal("fill", range = c("steelblue", "purple"), label=c("Male", "Female")) %>%
+      add_axis("x", title = "", properties = axis_props(labels=list(angle=270, align="right"))) %>%
+      add_axis("y", title = "", format='d') %>% 
+      add_legend("fill", title = "Gender") %>% 
+      set_options(width="auto") %>% 
+      add_tooltip(function(casualties) (casualties$stack_upr_ - casualties$stack_lwr_))
   })
+  ageband_gender %>% bind_shiny("ageband_gender") 
   
   # Casualties by ageband and gender
-  output$ageband_severity <- renderPlot({
-    ageband_severity <- casualties() %>%
+  ageband_severity <- reactive({ 
+    casualties() %>%
       group_by(ageband, severity) %>%
-      summarise(count = n())
-    ggplot(ageband_severity, aes(x=ageband, y=count, fill=severity)) +
-      geom_bar(stat="identity") +
-      scale_y_continuous(breaks= pretty_breaks()) +
-      scale_fill_manual(values=c("black", "red", "orange"), name="", labels=c("Fatal", "Serious", "Slight")) +
-      ylab("") + xlab("") +
-      theme_bw() +
-      theme(legend.position = "bottom",
-            axis.line = element_line(colour = "black"),
-            panel.border = element_rect(colour = NA),
-            axis.text.x = element_text(angle = 90, hjust = 1),
-            panel.grid.major.x = element_line(colour=NA))
+      summarise(count = n()) %>% 
+      ggvis(~ageband, ~count, fill = ~severity) %>%
+      layer_bars(stroke := "white") %>%
+      scale_nominal("fill", range = c("black", "red", "orange"), label=c("Fatal", "Serious", "Slight")) %>%
+      add_axis("x", title = "", properties = axis_props(labels=list(angle=270, align="right"))) %>%
+      add_axis("y", title = "", format='d') %>% 
+      add_legend("fill", title = "Severity") %>% 
+      set_options(width="auto") %>% 
+      add_tooltip(function(casualties) (casualties$stack_upr_ - casualties$stack_lwr_))
   })
+  ageband_severity %>% bind_shiny("ageband_severity") 
+  
   
   # RAW DATA ##
   
   output$table <- DT::renderDataTable({
-    casualties() %>% select(AREFNO, date, mode, severity, agegroup, borough, long, lat)
+    casualties() %>% select(AREFNO, date, mode, severity, sex, ageband, borough, long, lat)
     
   }) }
 
